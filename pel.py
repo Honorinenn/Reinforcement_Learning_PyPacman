cbdc557f133799584861abfd36cbab9e713a5b07
Honorine Ndom Ndzah <honorinendzah@gmail.com>
2025-08-19 18:23:08 -0700

    removed pel.py

--- pel.py
+++
@@ -1,8 +0,0 @@
-from layout import getLayout
-
-# Load the layout from a file named 'tinyFood.lay'
-# Assuming 'layouts' directory is accessible.
-my_layout = getLayout('tinyFood')
-
-# Now 'my_layout' holds all the information about our tiny maze!
-print("Layout loaded!")
